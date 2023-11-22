49240c0f610f3b34166278f3522d438ba7082d7a
djanyreason <djanyreason@gmail.com>
2023-11-22 12:56:02 -0500

    Completes ex. 13.4, makes app web/HTTP-based
    This commit transforms the application into a web-based application
    relying upon express. It supports GET to /api/blogs, which responds
    with the list of blogs in the SQL database; POST to /api/blogs, which
    adds the request body to the database if formatted correctly, or
    responds with status code 400 and the error message if not; and
    DELETE to /api/blogs/:id, which destroys the entry at 'id', or if it
    does not exist responds with status code 404.

--- index.js
+++ index.js
@@ -1,20 +1,77 @@
 require('dotenv').config();
-const { Sequelize, QueryTypes } = require('sequelize');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const express = require('express');
+const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    const blogs = await sequelize.query('SELECT * FROM blogs', {
-      type: QueryTypes.SELECT
-    });
-    blogs.map((blog) =>
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    );
-    sequelize.close();
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+class Blog extends Model {}
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    author: {
+      type: DataTypes.TEXT
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog'
   }
-};
+);
 
-main();
+Blog.sync();
+
+app.use(express.json());
+
+app.get('/api/blogs', async (req, res) => {
+  try {
+    const blogs = await Blog.findAll();
+    console.log(JSON.stringify(blogs, null, 2));
+    res.json(blogs);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    await blog.destroy();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post('/api/blogs', async (req, res) => {
+  console.log(req.body);
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
