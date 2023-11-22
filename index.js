b0567b263d2fd099d8c01ff0bf4d5a2e912bada8
djanyreason <djanyreason@gmail.com>
2023-11-22 11:21:41 -0500

    Completes ex 13.3, prints database contents

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
 const { Sequelize, QueryTypes } = require('sequelize');
-const express = require('express');
-const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-app.get('/api/notes', async (req, res) => {
-  const notes = await sequelize.query('SELECT * FROM notes', {
-    type: QueryTypes.SELECT
-  });
-  res.json(notes);
-});
+const main = async () => {
+  try {
+    const blogs = await sequelize.query('SELECT * FROM blogs', {
+      type: QueryTypes.SELECT
+    });
+    blogs.map((blog) =>
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    );
+    sequelize.close();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+main();
