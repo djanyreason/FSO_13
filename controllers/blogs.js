51576c7f640807589f04e72d201fd5147a56c4c5
djanyreason <djanyreason@gmail.com>
2023-11-24 18:57:23 -0500

    Completes ex. 13.14, expands search to author
    This commit expands the search query to search both blog title and
    author.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,9 +7,14 @@ const { Blog, BloglistUser } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
-    const where = {};
-    if (req.query.search) where.title = { [Op.iLike]: `%${req.query.search}%` };
-    console.log(req.query.search, where);
+    let where = {};
+    if (req.query.search)
+      where = {
+        [Op.or]: [
+          { title: { [Op.iLike]: `%${req.query.search}%` } },
+          { author: { [Op.iLike]: `%${req.query.search}%` } }
+        ]
+      };
     const blogs = await Blog.findAll({
       attributes: { exclude: ['bloglistuserId'] },
       include: {
