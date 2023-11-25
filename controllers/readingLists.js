ad1806dd4a8e668a086eda8be9f75a9eb1d2193e
djanyreason <djanyreason@gmail.com>
2023-11-25 11:08:45 -0500

    Completes ex. 13.22, adds PUT to readinglist read
    This commit adds a PUT route to /api/readinglists/:id, which allows
    a logged-in user to update the 'read' status of items on their
    readinglist.

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,10 +1,45 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const { SECRET } = require('../util/config');
 const { ReadingList } = require('../models');
 
 router.post('/', async (req, res, next) => {
   try {
     const { blogId, bloglistuserId } = req.body;
     const readingList = await ReadingList.create({ blogId, bloglistuserId });
+    res.json(readingList);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put('/:id', async (req, res, next) => {
+  if (!req.token)
+    return res
+      .status(401)
+      .json({ error: 'User associated with reading list must be logged in' });
+
+  const readingList = await ReadingList.findByPk(req.params.id);
+  if (!readingList) return res.status(404).end();
+
+  try {
+    const decodedToken = jwt.decode(req.token, SECRET);
+    if (decodedToken.id !== readingList.bloglistuserId)
+      return res
+        .status(401)
+        .json({ error: 'User associated with reading list must be logged in' });
+  } catch (error) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+
+  if (!Object.keys(req.body).includes('read'))
+    return res.status(200).json(readingList);
+
+  try {
+    readingList.read = req.body.read;
+    await readingList.save();
     res.json(readingList);
   } catch (error) {
     next(error);
